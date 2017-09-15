28fff99195274255134369f85f0fd295f33ed37a
SymbolLong <877857078@qq.com>
2017-09-15 10:44:21 +0800

    初始化

---
+++ src/main/java/com/zhangsl/KentApplication.java
@@ -0,0 +1,12 @@
+package com.zhangsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zhangsl/KentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhangsl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
